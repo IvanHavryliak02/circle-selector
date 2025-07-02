ef5dda4f1f2361a3094dc607502f52f3cb5eec67
Ivan <ivan.webdevelp@gmail.com>
2025-07-02 16:17:18 +0200

    Moveout children works

--- src/js/modules/circle-selector.js
+++ src/js/modules/circle-selector.js
@@ -2,132 +2,99 @@
 export default function(containerSelector){
     //Execution code
     const selector = document.querySelector(containerSelector);
-    const selectorActivator = container.querySelector(`${containerSelector}__activator`);
-    const elements = container.querySelectorAll(`${containerSelector}__element`);
+    const selectorActivator = selector.querySelector(`${containerSelector}__activator`);
+    const elements = selector.querySelectorAll(`${containerSelector}__element`);
     
-
+    const primaryData = getSelectorParams(selector); //object
+    setSelectorSize(primaryData, selector); //sets container size
+    addPrimaryData(primaryData, selector, elements) //adds to primaryData radius, elementsAmount
+    const elementsData = calculateCoords(primaryData, selector, elements); //obj with target coords, element coords and element
     //functions
+
+    selectorActivator.addEventListener('mouseenter', () => {
+        function moveItems(){
+            elementsData.forEach(obj => {
+            const currentElement = obj.element;
+            currentElement.style.display = 'block';
+            const diffX = Math.abs(obj.targetCornerX - obj.elementCornerX);
+            const diffY = Math.abs(obj.targetCornerY - obj.elementCornerY);
+            const stepX = diffX/100;
+            const stepY = diffY/100;
+                if(obj.elementCornerX !== obj.targetCornerX){
+                    if(obj.elementCornerX < obj.targetCornerX){
+                        obj.elementCornerX += stepX;
+                        currentElement.style.left = `${obj.elementCornerX}px`;
+                    }else if(obj.elementCornerX > obj.targetCornerX){
+                        obj.elementCornerX -= stepX;
+                        currentElement.style.left = `${obj.elementCornerX}px`;
+                    }
+                }
+                if(obj.elementCornerY !== obj.targetCornerY){
+                    if(obj.elementCornerY < obj.targetCornerY){
+                        obj.elementCornerY += stepY;
+                        currentElement.style.top = `${obj.elementCornerY}px`;
+                    }else if(obj.elementCornerY > obj.targetCornerY){
+                        obj.elementCornerY -= stepY;
+                        currentElement.style.top = `${obj.elementCornerY}px`;
+                    }
+                }
+            })
+            requestAnimationFrame(moveItems);
+        }
+        moveItems();
+    })
 
     function getSelectorParams(selector){
-        
+        const startDeg = selector.dataset.startDeg, //string
+              size = selector.dataset.sizeInPx; //string
+              return {startDeg: startDeg, size: size};
     }
 
-    //console.log(container, selectorActivator, elements) //debug cheked - ok
-
-    /* const containerWidth = container.dataset.size,
-          containerHeight = container.dataset.size,
-          startDeg = parseFloat(container.dataset.startDeg),
-          radius = 0.8*(parseFloat(container.dataset.size)/2),
-          centerX = parseFloat(containerWidth)/2,
-          centerY = parseFloat(containerHeight)/2;
-
-          console.log(`Container:{w:${containerWidth}, h:${containerHeight}}`); //debug checked - ok
-          console.log(`StartDeg:${startDeg}`);
-          console.log(`Radius:${radius}`);
-          console.log(`Center:{x:${centerX}, y:${centerY}}`);
-
-    container.style.width = containerWidth;
-    container.style.height = containerHeight;
-
-    console.log(`calling initCalculation:`) //debug ok
-
-    const targetsObj = initCalculation(startDeg, radius);
-    
-    selectorActivator.addEventListener('mouseenter', () => {
-        const moveElements = moveOutElement(0);
-        let i = 0;
-        function step(){
-            moveElements();
-            i++;
-            if(i < 200){
-                requestAnimationFrame(step);
-            }
-        }
-        step();
-    });
-
-    //functions
-
-    function initCalculation(startDeg, radius){
-
-        const degsToRads = degs => degs*(Math.PI/180),
-              elementsAmount = elements.length,
-              startRadian = Math.round(degsToRads(startDeg)*1000)/1000,
-              radianBetweenElements = Math.round(degsToRads(360/elementsAmount)*1000)/1000,
-              result = [];
-
-        console.log(`Elements amount: ${elementsAmount}`); //debug ok
-        console.log(`Start rad: ${startRadian}`);
-        console.log(`Radian between elements: ${radianBetweenElements}`);
-
-        for(let i = 0; i < elementsAmount; i++){
-            const coords = {
-                element: elements[i],
-                targetCenterX: centerX + radius*Math.cos(startRadian + radianBetweenElements*i), 
-                targetCenterY: centerY + radius*Math.sin(startRadian + radianBetweenElements*i),
-                elementWidth: parseFloat(getComputedStyle(elements[i]).width),
-                elementHeight: parseFloat(getComputedStyle(elements[i]).height),
-                offsets: calculateElementCoordinates(elements[i])
-            };
-            result.push(coords);
-            
-        }
-        console.log(`function finished! Result:`); //debug ok
-        console.log(result)
-        return result;
+    function setSelectorSize(primaryData, selector){
+        const width = primaryData.size, //string
+              height = primaryData.size; //string
+        selector.style.width = width;
+        selector.style.height = height;
     }
 
-    function moveOutElement(i){
-        console.log('Mouse in, calling moveOutElement');
+    function addPrimaryData(primaryData, selector, elements){ //data is array with strings
+        const selectorWidth = parseFloat(selector.style.width)/2,
+        radius = 0.7 * selectorWidth;
+        primaryData.radius = radius;
+        primaryData.elementsAmount = elements.length;
+    }
 
-        const currentTarget = targetsObj[i];
-              console.log(currentTarget);
-
-        let cornerX = currentTarget.offsets[0],
-            cornerY = currentTarget.offsets[1];
-
-        const cornerTargetX = currentTarget.x - currentTarget.elementWidth/2,
-              cornerTargetY = currentTarget.y + currentTarget.elementHeight/2,
-              diffX = Math.abs(cornerTargetX - cornerX),
-              diffY = Math.abs(cornerTargetY - cornerY);
-              currentTarget.element.style.display = 'block'
-
-        return function (){
-            const stepX = diffX / 100,
-                  stepY = diffY / 100;
-            let newX, newY; 
+    function calculateCoords(primaryData, selector, elements){
+        const result = []
+        const degToRad = deg => deg*(Math.PI / 180);
+        elements.forEach((element,i) => {
+            const elementWidth = parseFloat(getComputedStyle(element).width);
+            const elementHeight = parseFloat(getComputedStyle(element).height);
+            const elementLeftProc = parseFloat(selector.dataset.childrenLeft);
+            const elementTopProc = parseFloat(selector.dataset.childrenTop);
+            const parentWidth = parseFloat(getComputedStyle(selector).width);
+            const parentHeight = parseFloat(getComputedStyle(selector).height);
+            const offsetLeft = elementLeftProc/100 * parentWidth - elementWidth/2; //elementCornerX
+            const offsetTop = elementTopProc/100 * parentHeight - elementHeight/2; //elementCornerY
+            const centerX = parentWidth/2;
+            const centerY = parentHeight/2;
             
-            if(cornerTargetX > 0 && cornerTargetY >= 0){
-                newX = cornerX + stepX;
-                newY = cornerY + stepY
-            }else if(cornerTargetX <= 0 && cornerTargetY > 0){
-                newX = cornerX - stepX;
-                newY = cornerY + stepY
-            }else if(cornerTargetX < 0 && cornerTargetY <= 0){
-                newX = cornerX - stepX;
-                newY = cornerY - stepY
-            }else if(cornerTargetX >= 0 && cornerTargetY < 0){
-                newX = cornerX + stepX;
-                newY = cornerY - stepY
-            }
-            currentTarget.element.style.left = `${newX}px`
-            currentTarget.element.style.top = `${newY}px`
-            cornerX = newX;
-            cornerY = newY;
-        }
-    } 
+            const n = primaryData.elementsAmount;
+            const startRad = degToRad(primaryData.startDeg);
+            const R = primaryData.radius;
+            const targetCenterX = centerX + (R * Math.cos(startRad + (2*Math.PI/n) * i)); 
+            const targetCenterY = Math.abs(-centerY + (R * Math.sin(startRad + (2*Math.PI/n) * i)));
+            const targetCornerX = targetCenterX - elementWidth/2;
+            const targetCornerY = targetCenterY - elementHeight/2;
 
-    function calculateElementCoordinates(element){
-              
-        const parentWidth = parseFloat(containerWidth),
-              parentHeight = parseFloat(containerHeight),
-              elementTopProc = parseFloat(getComputedStyle(element).top),
-              elementLeftProc = parseFloat(getComputedStyle(element).left),
-              elementWidth = parseFloat(getComputedStyle(element).width),
-              elementHeight = parseFloat(getComputedStyle(element).height);
-              console.log(`ParentWidth: `)
-        const offsetLeft = elementLeftProc/100 * parentWidth - elementWidth/2,
-              offsetTop = elementTopProc/100 * parentHeight - elementHeight/2;
-        return {elementCornerX: offsetLeft, elementCornerY:offsetTop };
-    } */
+            result.push({
+                element: element,
+                elementCornerX: offsetLeft,
+                elementCornerY: offsetTop,
+                targetCornerX: targetCornerX,
+                targetCornerY: targetCornerY
+            });
+        })
+        return result
+    }
 }
